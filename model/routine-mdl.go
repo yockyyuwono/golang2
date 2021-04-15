1041ab230a0a59f5c619dec229ec123c459349ee
yockyyuwono <yockyyuwono@gmail.com>
2021-04-15 17:10:36 +0700

    add routine-mdl

---
+++ model/routine-mdl.go
@@ -0,0 +1,14 @@
+package model
+
+type RoutineSave_mdl struct {
+	RowId           int
+	Value1          string
+	Value2          string
+	Value3          string
+	Status          string
+	Active          string
+	CreatedBy       string
+	CreatedDate     string
+	LastUpdatedBy   string
+	LastUpdatedDate string
+}
